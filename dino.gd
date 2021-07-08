d23e226c5ba1654741b1e0210270c879748cd9f2
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-08 23:49:25 +0530

    Need to fix double jump issue

--- dino.gd
+++ dino.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+
 const GRAVITY = 10
 const JUMP_SPEED = -400
 var velocity = Vector2.ZERO
@@ -6,7 +7,9 @@ var velocity = Vector2.ZERO
 func _process(delta):
 	velocity.y += GRAVITY
 	
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_SPEED
 		$Sprite.play("jump")
-	move_and_slide(velocity)
+	if Input.is_action_just_released("jump") and not is_on_floor():
+		velocity.y = 0
+	move_and_slide(velocity,Vector2.UP)
