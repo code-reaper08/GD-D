2c862ecfefe12f04881b45d0031e207c9d610ed3
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-07-10 23:27:53 +0530

    Added score calculator

--- Level.gd
+++ Level.gd
@@ -1,6 +1,18 @@
 extends Node
 
 const ENEMY = preload("res://enemy.tscn")
+
+
+func _ready():
+	Global.currentscore = 0
+
 func _on_Timer_timeout():
 	var enemy =ENEMY.instance()
 	add_child(enemy)
+
+
+func _on_Scorecalc_timeout():
+	Global.currentscore += 1
+	
+func _process(delta):
+	$Label.text = "errors survived : " + str(Global.currentscore)
--- titlescreen.gd
+++ titlescreen.gd
@@ -1,6 +1,8 @@
 extends Control
 
 func _ready():
+	if Global.currentscore > Global.highscore:
+		Global.highscore = Global.currentscore
 	$Highscore.text = "Highcscore : " + str(Global.highscore)
 func _process(delta):
 	if Input.is_action_just_pressed("jump"):
